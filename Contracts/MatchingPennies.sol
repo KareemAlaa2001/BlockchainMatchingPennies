47444b14caba2114a5d5cddf0118c2ac7026ce6d
KareemAlaa2001 <kareemalaa2001@gmail.com>
2021-10-31 13:31:04 +0000

    added support for protecting the game against griefing

--- Contracts/MatchingPennies.sol
+++ Contracts/MatchingPennies.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AFL-3.0
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract MatchingPennies {
 
@@ -19,21 +19,33 @@ contract MatchingPennies {
     mapping(address => bytes32) commitments;
     address[] private players;
     Vote[] votes;
+    uint private revealPhaseEndTime;
 
     fallback() external {
         emit InvalidFunctionCall("An invalid function was called in this contract! Fallback triggered instead.");
     }
 
     function joinGame(bytes32 commitment) public payable {
-        require(players.length < 2, "Game already full! Please join when this round is over!");
+        //  either we are in a clean slate new game or we are resetting a previously full game only AFTER the game time ran out.
+        require(players.length < 2 || (revealPhaseEndTime != 0 && revealPhaseEndTime < block.timestamp), "Game already full! Please join when this round is over!");
         require(commitments[msg.sender] == 0, "Can't play the game against yourself!");
         require(msg.value == 1 ether, "Amount sent is NOT 1 ether!");
+
+        //  this means that joinGame was called to reset the game state after the previous game ended with both players griefing
+        if (revealPhaseEndTime != 0) {
+            //  need to reset the state. 
+            assert(players.length == 2);
+            resetState();
+        }
 
         commitments[msg.sender] = commitment;
         players.push(msg.sender);
         emit PlayerJoined("A new player has joined the game");
 
-        if (players.length == 2) emit CommitPhaseOver("The commit phase is now order. Registered players can begin revealing their values.");
+        if (players.length == 2) {
+            revealPhaseEndTime = add(block.timestamp, 1 hours);
+            emit CommitPhaseOver("The commit phase is now order. Registered players can begin revealing their values. The reveal phase will end in 1 hour.");
+        }
     }
 
     function revealValue(bytes32 _commitment, string memory _vote) public {
@@ -42,6 +54,7 @@ contract MatchingPennies {
         require(commitments[msg.sender] == _commitment, "Invalid parameters!");
         require(commitments[msg.sender] == keccak256(abi.encodePacked(_vote)), "Invalid parameters!");
         require(votes.length < 2, "Both votes in this round already revealed!");
+        require(block.timestamp < revealPhaseEndTime, "Can't reveal a value after the reveal phase has ended!");
 
         if (votes.length == 1) {
             require(votes[0].player != msg.sender, "You have already revealed a value, can't do that again!");
@@ -53,27 +66,37 @@ contract MatchingPennies {
     }
 
     function calculateWinner() public {
-        require(votes.length == 2, "Not all votes have been revealed!");
-        
+        require(revealPhaseEndTime != 0, "Can't call calculateWinner() unless both players have joined the game!");
+        require(votes.length == 2 || block.timestamp > revealPhaseEndTime, "Not all votes have been revealed!");
+
         address winner; 
+        bool winnerFound = true;
 
-        if (votes[0].vote % 2 == votes[1].vote % 2) {
-            winner = players[0];
+        //  if nobody griefed and the game ended as expected.
+        if (votes.length == 2) {
+            if (votes[0].vote % 2 == votes[1].vote % 2) {
+                winner = players[0];
+            } else {
+                winner = players[1];
+            }
+        //  if one player griefed, reward the other player with an auto win
+        } else if (votes.length == 1) {
+            winner = votes[0].player;
+        //  if both players griefed, both get punished. Nobody gets their ether back.
+        //  need to keep this condition in case a different person calls this function
         } else {
-            winner = players[1];
+            winnerFound = false;
         }
 
-        //  need to reset all of my vars so that other people can join the game
-        for (uint8 i = 0; i < players.length; i++) {
-            commitments[players[i]] = 0;
+        resetState();
+
+        if (winnerFound) {
+            emit LogWinnerFound("A winner has been found!", winner);
+
+            //  using safemaths-esque add to support solidity version 0.7 overlflow safety
+            balances[winner] = add(balances[winner],2 ether);
         }
-        delete players;
-        delete votes;
-
-        emit LogWinnerFound("A winner has been found!", winner);
-
-        //  no need to use safeMaths. Solidity 0.8.0 supports checked operations by default.
-        balances[winner] += 2 ether;
+        
     }
 
     function withdrawBalance() public {
@@ -82,5 +105,20 @@ contract MatchingPennies {
         balances[msg.sender] = 0;
 
         payable(msg.sender).transfer(balance);
+    }
+
+    function resetState() private {
+        commitments[players[0]] = 0;
+        commitments[players[1]] = 0;
+
+        delete players;
+        delete votes;
+        delete revealPhaseEndTime;
+    }
+
+    function add(uint a, uint b) private pure returns (uint) {
+        uint c = a + b;
+        require(c >= a);
+        return c;
     }
 }
